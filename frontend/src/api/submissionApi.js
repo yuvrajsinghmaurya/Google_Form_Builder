e7608b1e35978c39a9d7661a3924d704fac5ca05
yuvraj singh <127769740+yuvrajsinghmaurya@users.noreply.github.com>
2025-11-22 09:12:35 +0530

    Change BASE_URL to production server

--- frontend/src/api/submissionApi.js
+++ frontend/src/api/submissionApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8090";
+const BASE_URL = "https://google-form-builder.onrender.com";
 
 // Submit form response (USER)
 export const submitFormResponse = async (formId, responses) => {
